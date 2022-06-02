fb362cd3570c15956d90bcecd1408b09263f5b3f
tseloteyonas <nomiyonas21@gmail.com>
2022-06-03 00:23:32 +0300

    update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
