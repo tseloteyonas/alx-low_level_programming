819588422b598ecc8658b0356718c2f5f54fed60
tseloteyonas <nomiyonas21@gmail.com>
2022-06-03 23:46:39 +0300

    update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'z' ; alp >= 'a' ; alp--)
+		putchar(alp);
+	putchar('\n');
+	return (0);
+}
