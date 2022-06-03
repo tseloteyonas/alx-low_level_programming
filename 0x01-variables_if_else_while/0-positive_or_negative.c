0715770b2de7734ae43592baf7f952e599f8776f
tseloteyonas <nomiyonas21@gmail.com>
2022-06-03 22:58:36 +0300

    update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,7 +17,7 @@ int main(void)
 		printf("%d is %s\n", n, "positive");}
 	else if(n < 0){
 		printf("%d is %s\n", n, "negative");}
-	else 
+	else {
 		printf("%d is %s\n", n, "zero");}
         return (0);
 }
