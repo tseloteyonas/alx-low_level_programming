8f18c7a49a3f05023bc957e953848393b69def24
tseloteyonas <nomiyonas21@gmail.com>
2022-06-03 22:16:13 +0300

    update

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdlib.h>
+int n;
+n=rand();
+if (n>0)
+{
+	printf(n is positive'\n');
+}
+if(n=0)
+	{
+        printf(n is zero'\n');
+	}
+	if(n<0)
+{
+        printf(n is negative'\n');
+}
+
