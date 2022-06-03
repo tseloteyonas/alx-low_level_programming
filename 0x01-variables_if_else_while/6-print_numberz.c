bddaea4180d210d815c4b68a8f08af25bbf24824
tseloteyonas <nomiyonas21@gmail.com>
2022-06-03 23:44:50 +0300

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: printing single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
