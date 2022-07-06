c2635c4126b47f05e6214329397dde3886b10ed0
tseloteyonas <nomiyonas21@gmail.com>
2022-07-06 22:41:15 +0300

    update

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FUNCPOINTER
+#define FUNCPOINTER
+#include <stddef.h>
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int _putchar(char c);
+int int_index(int *array, int size, int (*cmp)(int));
+#endif
